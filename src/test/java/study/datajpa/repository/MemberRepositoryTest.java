479379e55ff0a0a72d88215f8fbd5f455ec22f83
noname_dev <noname_dev@naver.com>
2022-11-17 19:20:40 +0900

    파라미터 바인딩

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -11,6 +11,7 @@ import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.entity.Team;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -129,8 +130,18 @@ class MemberRepositoryTest {
         assertThat(memberDto.get(0).getId()).isEqualTo(m1.getId());
         assertThat(memberDto.get(0).getUsername()).isEqualTo(m1.getUsername());
         assertThat(memberDto.get(0).getTeamName()).isEqualTo(team.getName());
-
     }
+    @Test
+    public void findByNames(){
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
 
+        List<Member> result = memberRepository.findByNames(Arrays.asList("AAA", "BBB"));
+        for (Member member : result) {
+            System.out.println("member = " + member);
+        }
+    }
 
 }
